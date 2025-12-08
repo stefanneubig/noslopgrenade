8d63f6c12dda2ebc776dc948297b1032ee42c297
Stefan Neubig <mail@stefanneubig.com>
2025-12-08 10:43:50 +0200

    Fix Pages worker format

--- _worker.js
+++ _worker.js
@@ -1,13 +1,11 @@
-export default {
-  async fetch(request) {
-    const url = new URL(request.url);
+export async function onRequest(context) {
+  const url = new URL(context.request.url);
 
-    // Redirect www to non-www
-    if (url.hostname === 'www.noslopgrenade.com') {
-      return Response.redirect(`https://noslopgrenade.com${url.pathname}${url.search}`, 301);
-    }
-
-    // Pass through to static assets
-    return fetch(request);
+  // Redirect www to non-www
+  if (url.hostname === 'www.noslopgrenade.com') {
+    return Response.redirect(`https://noslopgrenade.com${url.pathname}${url.search}`, 301);
   }
+
+  // Pass through to static assets
+  return context.next();
 }
